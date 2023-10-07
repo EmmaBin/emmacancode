eeb22b1e2c9ff45e98156fb428d53bb01c82108b
emmacodes <binma.emma@gmail.com>
2023-10-06 23:43:42 -0700

    made projects part working and adjusted to fit different screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -9,18 +10,30 @@ import {
 import AboutMe from './components/AboutMe/AboutMe'
 import Projects from './components/Projects/Projects';
 import Experience from './components/Experience';
-import Resume from './components/Resume';
+
 import MainLayout from './MainLayout/MainLayout';
 
 import './App.css';
+interface ExternalRedirectProps {
+  to: string;
+}
 
+const ExternalRedirect: React.FC<ExternalRedirectProps> = ({ to }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.open(to, '_blank');
+  }, [navigate, to]);
+
+  return null;
+};
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<AboutMe />} />
       <Route path="projects" element={<Projects />} />
       <Route path="experiences" element={<Experience />} />
-      <Route path="resume" element={<Resume />} />
+      <Route path="resume" element={<ExternalRedirect to="https://rxresu.me/emmacancode/bin-ma" />}/>
     </Route>
   )
 )
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -8,8 +8,19 @@ import './projects.css';
 
 
 const Projects = () => {
+
+
     const [currentFilter, setCurrentFilter] = React.useState('all');
     const [isSmallScreen, setIsSmallScreen] = React.useState(window.innerWidth <= 768);
+
+    React.useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
 
     const handleFilter = (filter: 'all' | 'personal' | 'group') => {
@@ -33,29 +44,66 @@ const Projects = () => {
                 {(currentFilter === 'all' || currentFilter === 'group') && (
                     <div className={`${currentFilter === 'all' ? 'small-img' : ''} ${currentFilter === 'group' ? 'group-projects-large-screen' : ''}`}>
                         <img src={SPEC} alt="spec-project" />
-                        <div className='overlay'>
-                            <span className='text'>Contracting work - using React, Tailwind, mobile first responsive. <a href="http://your-live-link.com">Live Link</a></span>
+                        <div className='text-overlay'>
+                            <div className='project-text'>
+                                Contract Work
+                                <br>
+                                </br>React, Tailwind, Mobile-First, Formik & Yup library, Unit Testing
+                                <br></br>
+                                <a href="https://specollective.org/" target="_blank" rel="noopener noreferrer">SEE LIVE</a></div>
                         </div>
                     </div>
                 )}
                 {(currentFilter === 'all' || currentFilter === 'personal') && (
                     <div className={currentFilter === 'all' ? 'small-img' : ''}>
                         <img src={quiz} alt="quiz" />
+                        <div className='text-overlay'>
+                            <div className='project-text'>
+                                Quizz App
+                                <br>
+                                </br>React,Trivia API
+                                <br></br>
+                                <a href="https://stately-malasada-cfd990.netlify.app/" target="_blank" rel="noopener noreferrer">SEE LIVE</a></div>
+                        </div>
                     </div>
                 )}
                 {(currentFilter === 'all' || currentFilter === 'group') && (
                     <div className={`${currentFilter === 'all' ? 'small-img' : ''} ${currentFilter === 'group' ? 'group-projects-large-screen' : ''}`}>
                         <img src={skyfall} alt="skyfall" />
+                        <div className='text-overlay'>
+                            <div className='project-text'>
+                                Skyfall
+                                <br>
+                                </br>JS, React, Tailwind, Jest Unit Testing, API
+                                <br></br>
+                                <a href="https://team27-skyfall.netlify.app/" target="_blank" rel="noopener noreferrer">SEE LIVE</a></div>
+                        </div>
                     </div>
                 )}
                 {(currentFilter === 'all' || currentFilter === 'personal') && (
                     <div className={currentFilter === 'all' ? 'small-img' : ''}>
                         <img src={dimsum} alt="dimsum" />
+                        <div className='text-overlay'>
+                            <div className='project-text'>
+                                Dim sum guru
+                                <br>
+                                </br>React, JS, Node.js, Express, PostgreSQL
+                                <br></br>
+                                <a href="https://github.com/EmmaBin/dimsum_guru" target="_blank" rel="noopener noreferrer">Github</a></div>
+                        </div>
                     </div >
                 )}
                 {(currentFilter === 'all' || currentFilter === 'personal') && (
                     <div className={currentFilter === 'all' ? 'small-img' : ''}>
                         <img src={movie} alt="movie" />
+                        <div className='text-overlay'>
+                            <div className='project-text'>
+                                Movie Watchlist
+                                <br>
+                                </br>JavaScript, OMDB API
+                                <br></br>
+                                <a href="https://jazzy-sawine-5d9be6.netlify.app/" target="_blank" rel="noopener noreferrer">SEE LIVE</a></div>
+                        </div>
                     </div>
                 )}
             </div>
